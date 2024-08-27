f5f5d46a63b45a0587c2d04a6f7f053cab47eed7
Bread_NiceCat <bread_nicecat@qq.com>
2024-08-27 17:18:49 +0800

    物品在创造模式物品栏中

--- common/src/main/java/cn/breadnicecat/lovemod/item/ModTabs.java
+++ common/src/main/java/cn/breadnicecat/lovemod/item/ModTabs.java
@@ -1,8 +1,10 @@
 package cn.breadnicecat.lovemod.item;
 
 import dev.architectury.registry.CreativeTabRegistry;
+import dev.architectury.registry.registries.RegistrySupplier;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.Item;
 
 /**
  * Created in 2024/8/27 13:03
@@ -16,8 +18,13 @@ import net.minecraft.world.item.CreativeModeTab;
 public class ModTabs {
 	public static final String TAB_I18N = "tab.lovemod.title";
 	public static final CreativeModeTab tab =
-			CreativeTabRegistry.create(
-					Component.translatable(TAB_I18N),
-					() -> ModItems.RING_WEDDING.get().getDefaultInstance()
+			CreativeTabRegistry.create(builder -> builder.title(Component.translatable(TAB_I18N))
+					.icon(() -> ModItems.RING_WEDDING.get().getDefaultInstance())
+					.displayItems((a, b) -> {
+						for (RegistrySupplier<Item> supplier : ModItems.register) {
+							b.accept(supplier.get());
+						}
+					})
+					.build()
 			);
 }
